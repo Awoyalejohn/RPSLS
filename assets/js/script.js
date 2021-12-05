9596f9271b8d7dfc8503be08f371836bdac27ed9
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2021-12-05 16:30:50 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+function runGame() {
+
+}
+
+function displayPlayerGestureChosen() {
+  
+}
+
+function displayComputerGestureChosen() {
+  
+}
+
+function determineRoundWinner() {
+  
+}
+
+function displayRoundWinner() {
+  
+}
+
+function incrementWin() {
+  
+}
+
+function incrementLose() {
+  
+}
+
+function incrementDraw() {
+  
+}
+
+function incrementRound() {
+  
+}
+
+function gameoverCondition() {
+  
+}
+
+function displayGameoverWinner() {
+  
+}
+
+
