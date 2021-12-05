af568cc04bbeabe65cfcde8278582a9b3a82adf2
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2021-12-05 17:11:40 +0000

    added game type attribute to all buttons and set up an event listener with the main game loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elemnts and add event listeners to them
+
+document.addEventListener('DOMContentLoaded', function() {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', function() {
+      if (this.getAttribute('data-type') === 'how to play') {
+        alert('You clicked How to play!');
+      } else {
+        let gametype = this.getAttribute('data-type');
+        alert(`You clicked ${gametype}`);
+      }
+
+    })
+
+  }
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
 function runGame() {
 
 }
